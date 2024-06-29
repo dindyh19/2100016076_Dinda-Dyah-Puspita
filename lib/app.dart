07a29fecdb9772e93241ba47f692d697d428ad76
agikik <agikik059@gmail.com>
2024-06-29 15:33:06 +0700

    commit

---
+++ lib/app.dart
@@ -0,0 +1,78 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'supplemental/cut_corners_border.dart';
+
+import 'home.dart';
+import 'login.dart';
+import 'colors.dart';
+
+// TODO: Convert ShrineApp to stateful widget (104)
+class ShrineApp extends StatelessWidget {
+  const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      initialRoute: '/login',
+      routes: {
+        '/login': (BuildContext context) => const LoginPage(),
+        // TODO: Change to a Backdrop with a HomePage frontLayer (104)
+        '/': (BuildContext context) => const HomePage(),
+        // TODO: Make currentCategory field take _currentCategory (104)
+        // TODO: Pass _currentCategory for frontLayer (104)
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+      },
+      // TODO: Add a theme (103)
+      theme: _kShrineTheme, // New code
+    );
+  }
+}
+
+// TODO: Build a Shrine Theme (103)
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePurple,
+      secondary: kShrinePurple,
+      error: kShrineErrorRed,
+    ),
+    scaffoldBackgroundColor: kShrineSurfaceWhite,
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrinePurple,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: CutCornersBorder(),
+      focusedBorder: CutCornersBorder(
+        borderSide: BorderSide(
+          width: 2.0,
+          color: kShrinePurple,
+        ),
+      ),
+      floatingLabelStyle: TextStyle(
+        color: kShrinePurple,
+      ),
+    ),
+  );
+}
+
+_buildShrineTextTheme(TextTheme textTheme) {}
+
+// TODO: Build a Shrine Theme (103)
+// TODO: Build a Shrine Text Theme (103)
